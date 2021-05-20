d1f8631160d1b00fa81afd82387f9fd220ac1b9d
4boysdontcry <hgy02166@gmail.com>
2021-05-20 18:01:50 +0900

    디자인 완료

--- public/js/talk.js
+++ public/js/talk.js
@@ -19,9 +19,13 @@ function onChangeAuth(r) {
 	if(user) {
 		$('.header-wrapper .email').text(user.email);
 		$('.header-wrapper .photo img').attr('src', user.photoURL);
+		$('.create-wrapper img').attr('src', user.photoURL);
 		$('.header-wrapper .info-wrap').css('display', 'flex');
 		$('.header-wrapper .logo i').css('display', 'none');
 		$('.create-wrapper').show();
+		$('.list-wrapper').show();
+		$('.gap-wrapper').css('flex-grow', '0');
+		$('.gap-wrapper .gap-wrap').css('display', 'none');
 		$('.create-wrapper input[name="writer"]').val(user.displayName);
 		$('.bt-login').css('display', 'none');
 		$('.bt-logout').css('display', 'flex');
@@ -29,9 +33,13 @@ function onChangeAuth(r) {
 	else {
 		$('.header-wrapper .email').text('');
 		$('.header-wrapper .photo img').attr('src', '//via.placeholder.com/1x1/333');
+		$('.create-wrapper img').attr('src', '//via.placeholder.com/1x1/fff');
 		$('.header-wrapper .info-wrap').css('display', 'none');
 		$('.header-wrapper .logo i').css('display', 'inline-block');
 		$('.create-wrapper').hide();
+		$('.list-wrapper').hide();
+		$('.gap-wrapper').css('flex-grow', '1');
+		$('.gap-wrapper .gap-wrap').css('display', 'flex');
 		$('.create-wrapper input[name="writer"]').val('');
 		$('.bt-login').css('display', 'flex');
 		$('.bt-logout').css('display', 'none');
